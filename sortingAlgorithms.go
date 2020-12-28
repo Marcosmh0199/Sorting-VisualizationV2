670f4a9375b1c1e6233e05b4b3b7cfa826efa38d
Marcos Méndez <marcosmh0199@gmail.com>
2020-12-28 13:58:06 -0600

    feat- backToConsole

--- sortingAlgorithms.go
+++ sortingAlgorithms.go
@@ -1,22 +1,54 @@
 /*
 Notas:
-
+1. Para una visualización correcta del gráfico de barras, por favor ejecutar el programa
+en una terminal a pantalla completa.
+2. Si se quiere cambiar el tamaño del slice, modificar la constante SLICE_SIZE.
 */
 
 package main
 
 import (
+	"fmt"
+	ui "github.com/gizak/termui/v3"
+	"github.com/gizak/termui/v3/widgets"
+	"github.com/lxn/win"
+	"log"
 	"math"
 	"math/rand"
+	"strconv"
+	"time"
 )
 
-const (
-	FONT_WIDTH = 8
-	FONT_HEIGHT = 16
-	MAX_NUMBER_SIZE = 32
-	MAX_PRIME = 101
-	MILI_SECONDS = 10
-)
+//Constants
+const FONT_WIDTH = 8
+const FONT_HEIGHT = 16
+const MAX_NUMBER_SIZE = 32
+const MAX_PRIME = 101
+const MILI_SECONDS = 10
+
+//global variables
+var width int = int(win.GetSystemMetrics(win.SM_CXSCREEN) / FONT_WIDTH)
+var height int = int(win.GetSystemMetrics(win.SM_CYSCREEN) / (FONT_HEIGHT*2))
+var bsChart widgets.BarChart
+var qsChart widgets.BarChart
+
+func main() {
+	fmt.Print("Indique el tamaño del slice: ")
+	var first int
+	fmt.Scanln(&first)
+	slice := randomSlice(MAX_PRIME, first)
+	//barChartDriver(slice)
+	if err := ui.Init(); err != nil {
+		log.Fatalf("failed to initialize termui: %v", err)
+	}
+	defer ui.Close()
+	initBsChart(slice)
+	initQsChart(slice)
+	ui.Render(&bsChart)
+	ui.Render(&qsChart)
+	bsChartDrawer(slice)
+	fmt.Scanln()
+}
 
 /*
 Generates a prime number from the system hour
@@ -28,15 +60,15 @@ func generateSeed() int{
 /*
 creates a N size slice with random numbers based on the linear congruential method using only the seed as param
 output: slice with N random integers
- */
-func randomSlice(seed int, size int) []int{
-	var slice = make([]int, size)
-	var m = int(math.Pow(2, MAX_NUMBER_SIZE)) // modulus
-	var a = 22695477                          // multiplier
-	var c = 1                                 //increment
+*/
+func randomSlice(seed int, size int) []float64{
+	var slice = make([]float64, size)
+	var m = int(math.Pow(2, MAX_NUMBER_SIZE))  // modulus
+	var a = 22695477                    // multiplier
+	var c = 1                           //increment
 	for i := 0; i < size; i++ {
 		seed = (a * seed + c) % m
-		slice[i] = seed % MAX_NUMBER_SIZE //cast to make the slice values compatible with the barChart
+		slice[i] = float64(seed % MAX_NUMBER_SIZE) //cast to make the slice values compatible with the barChart
 	}
 	return slice
 }
@@ -68,25 +100,83 @@ func bubbleSort(slice []float64, pair chan []int){
 	close(pair)
 }
 
-func quickSort(slice []float64, pair chan []int){
+func quickSort(slice []float64, pair chan []int) []float64 {
 	if len(slice) < 2 {
-		return
+		return slice
 	}
 	low := 0
 	high := len(slice)-1
 
 	pivot := rand.Int() % len(slice)
 
-	pair <- []int{pivot, high}
+	swap(&slice[pivot], &slice[high])
 	for i := range slice {
 		if slice[i] < slice[high] {
-			pair <- []int{i, low}
+			swap(&slice[i], &slice[low])
 			low++
 		}
 	}
-	pair <- []int{low, high}
+	swap(&slice[low], &slice[high])
 	quickSort(slice[:low], pair)
 	quickSort(slice[low+1:], pair)
-	//close(pair)
+	return slice
 }
 
+func qsChartDrawer(slice []float64){
+	qsChart.Data = make([]float64, len(slice))
+	copy(qsChart.Data, slice)
+	pairsChannel := make(chan []int)
+	go quickSort(qsChart.Data, pairsChannel)
+	for pair := range pairsChannel{
+		swap(&qsChart.Data[pair[0]], &qsChart.Data[pair[1]])
+		ui.Render(&qsChart)
+		time.Sleep(MILI_SECONDS * time.Millisecond)
+	}
+}
+
+func bsChartDrawer(slice []float64)  {
+	bsChart.Data = make([]float64, len(slice))
+	copy(bsChart.Data, slice)
+	pairsChannel := make(chan []int)
+	go bubbleSort(bsChart.Data, pairsChannel)
+	for pair := range pairsChannel{
+		swap(&bsChart.Data[pair[0]], &bsChart.Data[pair[1]])
+		ui.Render(&bsChart)
+		time.Sleep(MILI_SECONDS * time.Millisecond)
+	}
+}
+
+func generateLabels(slice []float64) []string {
+	var labels = make([]string, len(slice))
+	for i := range slice{
+		labels[i] = strconv.Itoa(i)
+	}
+	return labels
+}
+
+func initBsChart(slice []float64)  {
+	bsChart = *widgets.NewBarChart()
+	bsChart.Data = slice
+	bsChart.Title = "BubbleSort"
+	bsChart.SetRect(0, 0, width, height - 2)
+	bsChart.BarWidth = 3
+	bsChart.BarGap = 0
+	bsChart.Labels = generateLabels(slice)
+	bsChart.LabelStyles = []ui.Style{ui.NewStyle(ui.ColorWhite)}
+	bsChart.BorderBottom = false
+	bsChart.BarColors = []ui.Color{ui.ColorRed}
+	bsChart.NumStyles = []ui.Style{ui.NewStyle(ui.ColorBlack)}
+}
+
+func initQsChart(slice []float64){
+	qsChart = *widgets.NewBarChart()
+	qsChart.Data = slice
+	qsChart.Title = "QuickSort"
+	qsChart.SetRect(0, height-2, width, height*2 - 3)
+	qsChart.BarWidth = 3
+	qsChart.BarGap = 0
+	qsChart.Labels = generateLabels(slice)
+	qsChart.LabelStyles = []ui.Style{ui.NewStyle(ui.ColorWhite)}
+	qsChart.BarColors = []ui.Color{ui.ColorRed}
+	qsChart.NumStyles = []ui.Style{ui.NewStyle(ui.ColorBlack)}
+}
